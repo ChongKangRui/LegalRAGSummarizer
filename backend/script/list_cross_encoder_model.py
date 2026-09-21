b651c179b0ac0943e4c81833d51acfe78da18823
ChongKangRui <chongkangrui@gmail.com>
2026-09-21 15:26:16 +0800

    Measuring and trying to optimize the ram usage

---
+++ backend/script/list_cross_encoder_model.py
@@ -0,0 +1,8 @@
+
+
+
+from fastembed.rerank.cross_encoder import TextCrossEncoder
+
+
+print("=====================TextCrossEncoder Models=================")
+print(TextCrossEncoder.list_supported_models())
